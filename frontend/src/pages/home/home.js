2b977f22e9323caf5d5abc0a24a212256e783cfa
xitij27 <79000102+xitij27@users.noreply.github.com>
2023-02-26 09:58:33 +0800

    frontend updates

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './home.css'
+import Card from 'react-bootstrap/Card';
 
 const Home = () => {
   return (
     <div className='title'>
-        <h4>Home</h4>
+      <h2>Studying research papers made easier!</h2>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="./nltk.png" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
